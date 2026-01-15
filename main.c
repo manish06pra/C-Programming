1382c64c0a2e0dc55bebc3cd0a67634f25d5bc2c
Manish Prajapati <manish0019060@gmail.com>
2026-01-15 14:34:04 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+#include<string.h>
+#include<stdio.h>
+int main()
+{
+    char username[] = "manish";
+    char pass[]="man123";
+    char password[20];
+    printf("\nUsername: %s", username);
+    printf("\nEnter the password:");
+    scanf("%s",password);
+    if(strcmp(pass,password)==0)
+    {
+
+        printf("Validate");
+        printf("\nUsername: %s", username);
+        printf("\nPassword: %s", password);
+
+    }
+    else
+    {
+
+
+        printf("not valid");
+    }
+       return 0;
+}
