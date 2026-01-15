d105ad2b9d1c998aef3c6b51e23c8fe9f7e26f64
Manish Prajapati <manish0019060@gmail.com>
2026-01-15 14:49:38 +0530

    Add files via upload

---
+++ C Program to print the sum of main diagonal elements of a 3x3 matrix.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+
+int main()
+ {
+    int a[3][3], i, j, sum = 0;
+
+    printf("Enter 9 elements of the 3x3 matrix:\n");
+
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < 3; i++) {
+        sum += a[i][i];
+    }
+
+    printf("Sum of main diagonal elements = %d", sum);
+
+    return 0;
+}
