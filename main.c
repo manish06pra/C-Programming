4d5fd3ac20d7f60ecb381ead6bd3ad47f2da272a
Manish Prajapati <manish0019060@gmail.com>
2026-01-15 14:37:17 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a, b, temp;
+    int *p1, *p2;
+
+    printf("Enter First Number: ");
+    scanf("%d", &a);
+
+    printf("Enter Second Number: ");
+    scanf("%d", &b);
+
+    printf("Before Swapping: a = %d,b = %d\n", a, b);
+
+    p1 = &a;
+    p2 = &b;
+
+    temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+
+    printf("After Swapping: a = %d, b = %d", a, b);
+
+    return 0;
+}
