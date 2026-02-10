f1428f57c46a17c11671b4562ea119d29a2822f5
Manish Prajapati <manish0019060@gmail.com>
2026-02-10 11:04:23 +0530

    Add files via upload

---
+++ Program using input and output redirection in c program.c
@@ -0,0 +1,12 @@
+//Program:Demonstration of redirection
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char name[50];
+    scanf("%s",name);
+    printf("Name:%s\n",name);
+    return 0;
+}
